1e63b993e523980c030dc13e95f96a4af41ef3de
Gpanazio <panazio@gmail.com>
2025-12-17 15:29:58 -0300

    Create App component with BrickScrollytelling

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BrickScrollytelling from './components/BrickScrollytelling';
+
+function App() {
+  return (
+    <div className="App">
+      <BrickScrollytelling />
+    </div>
+  );
+}
+
+export default App;
