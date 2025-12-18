13ec61837f78310c29f5e3e71dbdfb6f1714002d
Gpanazio <panazio@gmail.com>
2025-12-18 11:59:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // FUNDAMENTAL
+  server: {
+    port: process.env.PORT || 5173, // Usa a porta do Railway ou 5173 localmente
+    host: true // Necessário para expor a rede no Docker/Railway
+  },
+  preview: {
+    port: 4173, // Define a porta fixa 4173 para o preview
+    host: true
+  }
 })
